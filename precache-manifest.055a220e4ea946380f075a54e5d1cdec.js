50e90a378eb934ca99c1a6a3d34fba37d2238e17
Alex Sapoznikov <alex@nodeswat.com>
2023-03-24 15:02:21 +0200

    Updates

---
+++ precache-manifest.055a220e4ea946380f075a54e5d1cdec.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16fc67c859e4f173a93a68894cb79e47",
+    "url": "./index.html"
+  },
+  {
+    "revision": "9868ef8b94a59c1cd789",
+    "url": "./static/js/2.24138a31.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.24138a31.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8ac53a2dc34820e27998",
+    "url": "./static/js/main.dcde0cfd.chunk.js"
+  },
+  {
+    "revision": "3514b0ddd2ed7cfbbfcf",
+    "url": "./static/js/runtime-main.d622e6e7.js"
+  }
+]);
